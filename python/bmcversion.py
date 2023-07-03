8f33cfd13ef3e3591d4e5e2e9c37b7c403437cd3
Jeff OConnor <35244024+jeffoconnorau@users.noreply.github.com>
2023-07-03 15:11:34 +1000

    Update bmcversion.py
    updating for authentication changes post 11.0.5 Management Console update

--- python/bmcversion.py
+++ python/bmcversion.py
@@ -1,6 +1,5 @@
-#!/usr/bin/python
-# script to get BMC version
-# Copyright 2022 Google LLC. All Rights Reserved.
+#!/usr/bin/python3
+# Copyright 2023 Google LLC. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,31 +13,61 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import sys
-import requests, json
-import google.oauth2.id_token
-import google.auth.transport.requests
-import os
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="/path/to/file.json"
+# This script has been updated in July 2023 - to Support the new
+# Authentication changes in version 11.0.5 of Google Cloud Backup & DR
+# To use this update the "key_path" location to your service account key file
+# update the "target_principal" with your service account id & also
+# update the "bmcname" to equal the hostname of your Management Console
+
+#!pip install google-auth
+
+from google.auth import impersonated_credentials
+from google.auth.transport.requests import Request
+from google.oauth2 import service_account
+
+# Path to your service account JSON key file
+key_path = '/path/to/file.json'
 
 # we are disabling SSL warnings, we could add SSL checking to remove need to do this
 import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-# hard coded BMC Name and oath2clientid   NEED TO BE UPDATED TO MATCH YOURS.   SA IS ASSUMED ACTIVATED SO NOT SPECIFIED
-bmcname = 'agm-1234.backupdr.actifiogo.com'
-oath2clientid = '1234-ABCDEF.apps.googleusercontent.com'
+# Load the service account credentials from the key file
+credentials = service_account.Credentials.from_service_account_file(key_path)
 
-#  token and session ID creation
+# Optional: Impersonate another service account if needed
+target_principal = 'python@<YOUR_PROJECT_NAME>.iam.gserviceaccount.com'
+target_scopes = ['https://www.googleapis.com/auth/cloud-platform']
+credentials = impersonated_credentials.Credentials(
+    source_credentials=credentials,
+    target_principal=target_principal,
+    target_scopes=target_scopes,
+    lifetime=3600,
+)
+
+# Refresh the token if it has expired
+if credentials.expired:
+    credentials.refresh(Request())
+
+# Get the access token
+access_token = credentials.token
+
+#bmcname = 'bmc-1234-xxxyyyzz-dot-asia-southeast1.backupdr.googleusercontent.com'
+bmcname = 'bmc-<YOURPROJECT_ID>-<RANDOM_CHARS>-dot-<REGION_OF_MANAGEMENT_CONSOLE>.backupdr.googleusercontent.com'
+
+import sys
+import requests, json
+import google.auth.transport.requests
+
 def bmclogin():
     global combinedheader
+
     request = google.auth.transport.requests.Request()
-    token = google.oauth2.id_token.fetch_id_token(request, oath2clientid)
-    #print(token)
-   
-    # we now load the session ID into a global header to use for all future commands 
-    authheader = { 'Authorization' : 'Bearer ' + token }
-    #print(authheader)
+    token = access_token
+#    print(token)
+
+    authheader = {"Authorization": "Bearer " + token}
+#    print(authheader)
     url = 'https://' + bmcname +'/actifio/session/'
     payload = ""
     response = requests.request("POST", url, data=payload, headers=authheader, verify=False)
@@ -47,14 +76,8 @@ def bmclogin():
         # print(response.text)
         print("Login failed to get session")
         sys.exit(1)
-    #else:
-    #    print("Login succeeded")
-    #grab just the ID, which is our session ID
     session = json.loads(response.text)['id']
-    #print(session)
-    # we now load the session ID into a global header to use for all future commands 
     combinedheader = { 'Authorization' : 'Bearer ' + token,'backupdr-management-session' : 'Actifio ' + session }
-    #print(combinedheader)
 
 # to get the BMC version
 def getversion():
@@ -66,7 +89,6 @@ def getversion():
         print("Command failed")
         sys.exit(1)
     summary = json.loads(response.text)['summary']
-    #print(response.text)
     print(summary)
 
 bmclogin()
